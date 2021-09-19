6cb9ff04bfcec7d694367f0b0f9d16c658442351
Karanko Andrei <andr.karanko@gmail.com>
2021-09-19 19:19:48 +0300

    until css minimize 1h 52 min

--- src/analytics.js
+++ src/analytics.js
@@ -1,19 +1,21 @@
+import * as $ from 'jquery';
+
 function createAnalytics() {
     let counter = 0;
-    let isDestroyed = false;
+    let destroyed = false;
 
     const listener = () => counter++;
 
-    document.addEventListener('click', listener);
+    $(document).on('click', listener);
 
     return {
         destroy() {
-            document.removeEventListener('click', listener);
-            isDestroyed = true;
+            $(document).off('click', listener);
+            destroyed = true;
         },
 
         getClicks() {
-            if(isDestroyed) {
+            if(destroyed) {
                 return `Analytics is destroyed. Total clicks = ${counter}`;
             }
             return counter;
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,84 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+console.log('isDev', isDev);
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js',
+        main: './index.js',
+        analytics: './analytics.js',
     },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html',
-        })
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            
+                        },
+                    }, 
+                        'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader'],
+            },
+        ]
+    }
 }
